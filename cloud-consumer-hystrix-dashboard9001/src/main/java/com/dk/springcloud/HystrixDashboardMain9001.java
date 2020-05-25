15782346dd117f8fe14ee5b4d9563cf293e1915d
dongkai <k_luck1995@163.com>
2020-05-25 13:33:46 +0800

    Hystrix 服务监控  (需要新建一个用于监控的微服务，并且要改造需要被监控的服务的启动类)

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/dk/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,18 @@
+package com.dk.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * HystrixDashboard 图形化监控
+ *
+ * @author dk
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
