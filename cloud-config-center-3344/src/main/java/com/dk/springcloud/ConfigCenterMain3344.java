63915008bf07270d00cfae9261e0429f1c0e8479
kluck1995 <865153370@qq.com>
2020-05-26 14:18:06 +0800

    config 配置中心  服务端和客户端的配置

---
+++ cloud-config-center-3344/src/main/java/com/dk/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,20 @@
+package com.dk.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * config注册中心
+ *
+ * @author dk
+ */
+@SpringBootApplication
+@EnableConfigServer
+//@EnableEurekaClient
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/dk/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,18 @@
+package com.dk.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Config客户端
+ *
+ * @author dk
+ */
+@SpringBootApplication
+//@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
